f2f2f7ebb501faa17376b787600884a12be7ed9f
csh <csh@duksung.ac.kr>
2023-12-20 16:29:38 +0900

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO officers(rank, first_name, last_name) VALUES ('CAPTAIN', 'James1', 'Kirk1');
+INSERT INTO officers(rank, first_name, last_name) VALUES ('CAPTAIN', 'James2', 'Kirk2');
+INSERT INTO officers(rank, first_name, last_name) VALUES ('CAPTAIN', 'James3', 'Kirk3');
---
+++ src/test/java/org/example/dbtest/DbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
